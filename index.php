14fbb29bd7879c7a8062f000f4ec606532bce7f0
mabujo <jaaahn@gmail.com>
2013-11-03 01:08:29 +0100

    edit copy

--- index.php
+++ index.php
@@ -23,8 +23,8 @@
 	<div class="jumbotron">
 		<div class="container">
 			<h1>AceStream Guide</h1>
-			<p>AceStream is a peer-to-peer streaming video application, that allows you to view streams shared by other people.</p>
-			<p>Our guide will help you set AceStream, so you can enjoy your stream easily and in the best quality.</p>
+			<p>AceStream is a peer-to-peer streaming video application that allows you to view high quality video streams shared by other people.</p>
+			<p>Our guide will help you to setup AceStream, so you can enjoy your stream easily and in the best quality.</p>
 		</div>
 	</div>
 
